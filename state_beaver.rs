f04850e9e047787d168479c8fd12291f391e90e8
spaceybread <Spaceybread@gmail.com>
2024-03-26 22:41:15 -0500

    Update state_beaver.rs

--- state_beaver.rs
+++ state_beaver.rs
@@ -1,12 +1,39 @@
-fn next(current_state: i32, read_symbol: i8) -> (i32, i8, i8) {
-    
+fn next_4_2(current_state: i32, read_symbol: i8) -> (i32, i8, i8) {
+    // 4 states, 2 symbols
     // (next state, write symbol, shift)
-    // A: 0, B: 1, C: 2, D: 3, HALT: -1
-    // L: -1, R: 1
+    let A: i32 = 0;
+    let B: i32 = 1; 
+    let C: i32 = 2; 
+    let D: i32 = 3; 
+    let X: i32 = -1;
 
+    let L: i8 = -1;
+    let R: i8 = 1;
+
+    // (next state, write symbol, shift)
     let state_table: Vec<Vec<(i32, i8, i8)>> = vec![
-        vec![(1, 1, 1), (0, 1, -1), (-1, 1, 1), (3, 1, 1)],
-        vec![(1, 1, -1), (2, 0, -1), (3, 1, -1), (0, 0, 1)],
+        vec![(B, 1, R), (A, 1, L), (X, 1, R), (D, 1, R)],
+        vec![(B, 1, L), (C, 0, L), (D, 1, L), (A, 0, R)],
+    ];
+
+    return state_table[read_symbol as usize][current_state as usize];
+}
+
+fn next_3_2(current_state: i32, read_symbol: i8) -> (i32, i8, i8) {
+    // 3 states, 2 symbols
+    // (next state, write symbol, shift)
+    let A: i32 = 0;
+    let B: i32 = 1; 
+    let C: i32 = 2; 
+    let X: i32 = -1;
+
+    let L: i8 = -1;
+    let R: i8 = 1;
+
+    // (next state, write symbol, shift)
+    let state_table: Vec<Vec<(i32, i8, i8)>> = vec![
+        vec![(B, 1, R), (C, 0, R), (C, 1, L)],
+        vec![(X, 1, R), (B, 1, R), (A, 1, L)],
     ];
 
     return state_table[read_symbol as usize][current_state as usize];
@@ -21,7 +48,7 @@ fn main() {
     
     while state != -1 {
         let change: (i32, i8, i8);
-        change = next(state, tape[tape_pos as usize]); // find the next state
+        change = next_4_2(state, tape[tape_pos as usize]); // find the next state
         
         tape[tape_pos as usize] = change.1;
         tape_pos += change.2 as i32; 
